fcb93eab7da17cd4061937eb946145467e20c195
Unknown <findryankurnia@gmail.com>
2018-10-17 11:18:47 +0700

    Gambar Kotak

---
+++ DrawingToolkit/DrawingToolkit/Shape/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+
+namespace DrawingToolkit.Shape
+{
+    public class Rectangle : DrawingObject
+    {
+        public int rectX { get; set; }
+        public int rectY { get; set; }
+        public int rectWidth { get; set; }
+        public int rectHeight { get; set; }
+
+        private Pen pen;
+
+        public Rectangle()
+        {
+            this.pen = new Pen(Color.Black);
+        }
+
+        public Rectangle(int initX, int initY) : this()
+        {
+            this.rectX = initX;
+            this.rectY = initY;
+        }
+
+        public Rectangle(int initX, int initY, int initWidth, int initHeight) : this(initX, initY)
+        {
+            this.rectHeight = initHeight;
+            this.rectWidth = initWidth;
+        }
+
+        public override void Draw()
+        {
+            this.graphics.DrawRectangle(pen, rectX, rectY, rectWidth, rectHeight);
+        }
+    }
+}
