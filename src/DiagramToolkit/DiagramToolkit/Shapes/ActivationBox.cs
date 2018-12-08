406457c7ff77624c778ca66cd8af6d07b35d483e
Unknown <findryankurnia@gmail.com>
2018-12-08 17:53:32 +0700

    FP/add ActivationBox and ObjejtMessage

---
+++ src/DiagramToolkit/DiagramToolkit/Shapes/ActivationBox.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace DiagramToolkit.Shapes
+{
+    public class ActivationBox : DrawingObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        private Pen pen;
+
+        public int xTest;
+        public int yTest;
+        public int wTest;
+        public int hTest;
+
+        public ActivationBox()
+        {
+            this.pen = new Pen(Color.Black);
+            pen.Width = 1.5f;
+        }
+
+        public ActivationBox(int x, int y) : this()
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public ActivationBox(int x, int y, int width, int height) : this(x, y)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override void RenderOnPreview()
+        {
+            this.pen = new Pen(Color.Red);
+            pen.Width = 1.5f;
+            pen.DashStyle = DashStyle.DashDotDot;
+            if (this.Graphics != null)
+            {
+                this.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                this.Graphics.DrawRectangle(pen, X, Y, Width, Height);
+                drawLine();
+            }
+        }
+
+        public override void RenderOnEditingView()
+        {
+            this.pen = new Pen(Color.Blue);
+            pen.Width = 1.5f;
+            if (this.Graphics != null)
+            {
+                this.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                this.Graphics.DrawRectangle(pen, X, Y, Width, Height);
+                drawLine();
+            }
+        }
+
+        public override void RenderOnStaticView()
+        {
+            this.pen = new Pen(Color.Black);
+            pen.Width = 1.5f;
+            pen.DashStyle = DashStyle.Solid;
+            if (this.Graphics != null)
+            {
+                this.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                this.Graphics.DrawRectangle(pen, X, Y, Width, Height);
+                drawLine();
+            }
+        }
+
+        public override bool Intersect(int xTest, int yTest)
+        {
+            if ((xTest >= X && xTest <= X + Width) && (yTest >= Y && yTest <= Y + Height))
+            {
+                Debug.WriteLine("Object " + ID + " is selected.");
+                return true;
+            }
+            return false;
+        }
+
+        public override void Translate(int x, int y, int xAmount, int yAmount)
+        {
+            this.X += xAmount;
+            this.Y += yAmount;
+        }
+
+        public Point Startpoint;
+        public Point Endpoint;
+
+        public void drawLine()
+        {
+            this.pen = new Pen(Color.Red);
+            pen.Width = 1.5f;
+            pen.DashStyle = DashStyle.DashDotDot;
+            xTest = (Width / 2) + X;
+            yTest = Height + Y;
+            hTest = Width + Width;
+            this.Graphics.DrawRectangle(pen, xTest, yTest, 1, hTest);
+        }
+    }
+}
---
+++ src/DiagramToolkit/DiagramToolkit/Shapes/ObjectMessage.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace DiagramToolkit.Shapes
+{
+    public class ObjectMessage : DrawingObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        private Pen pen;
+
+        public int xTest;
+        public int yTest;
+        public int wTest;
+        public int hTest;
+
+        public ObjectMessage()
+        {
+            this.pen = new Pen(Color.Black);
+            pen.Width = 1.5f;
+        }
+
+        public ObjectMessage(int x, int y) : this()
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public ObjectMessage(int x, int y, int width, int height) : this(x, y)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override void RenderOnPreview()
+        {
+            this.pen = new Pen(Color.Red);
+            pen.Width = 1.5f;
+            pen.DashStyle = DashStyle.DashDotDot;
+            if (this.Graphics != null)
+            {
+                this.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                this.Graphics.DrawRectangle(pen, X, Y, Width, 1);
+                drawBox();
+            }
+        }
+
+        public override void RenderOnEditingView()
+        {
+            this.pen = new Pen(Color.Blue);
+            pen.Width = 1.5f;
+            if (this.Graphics != null)
+            {
+                this.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                this.Graphics.DrawRectangle(pen, X, Y, Width, 1);
+                drawBox();
+            }
+        }
+
+        public override void RenderOnStaticView()
+        {
+            this.pen = new Pen(Color.Black);
+            pen.Width = 1.5f;
+            pen.DashStyle = DashStyle.Solid;
+            if (this.Graphics != null)
+            {
+                this.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                this.Graphics.DrawRectangle(pen, X, Y, Width, 1);
+                drawBox();
+            }
+        }
+
+        public override bool Intersect(int xTest, int yTest)
+        {
+            if ((xTest >= X && xTest <= X + Width) && (yTest >= Y && yTest <= Y + Height))
+            {
+                Debug.WriteLine("Object " + ID + " is selected.");
+                return true;
+            }
+            return false;
+        }
+
+        public override void Translate(int x, int y, int xAmount, int yAmount)
+        {
+            this.X += xAmount;
+            this.Y += yAmount;
+        }
+
+        public Point Startpoint;
+        public Point Endpoint;
+
+        public void drawBox()
+        {
+            this.pen = new Pen(Color.Red);
+            pen.Width = 1.5f;
+            pen.DashStyle = DashStyle.Solid;
+            xTest = Width + X;
+            this.Graphics.DrawRectangle(pen, xTest, Y, 10, 30);
+        }
+    }
+}
